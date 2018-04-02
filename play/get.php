e191ef9202bedb503427e10f418cf0ee032f4b8c
Cheng <idevs@foxmail.com>
2018-04-02 09:38:25 +0800

    修复缓存问题

--- play/get.php
+++ play/get.php
@@ -7,14 +7,13 @@
     if(!empty($_GET['id'])){
         header('Content-Type: application/json;charset=UTF-8');
         
-        $play_list = $netease->playlist($_GET['id']);
-        
         $name = $_GET['id'] . ".json";
         $save = "cache/" . $name;
         if (file_exists($save)) {
             $fo = fopen($save, "r+");
             $ti = filemtime($save);
             if(date("Ymd", $ti) != date("Ymd")){
+                $play_list = $netease->playlist($_GET['id']);
                 unlink($uri);
                 $str = str_replace("http:\/\/","https:\/\/", $play_list);
                 // $str = str_replace("http://m","https://p", $str);
@@ -24,6 +23,7 @@
                 echo fread($fo, filesize($save));
             }
         } else {
+            $play_list = $netease->playlist($_GET['id']);
             $str = str_replace("http:\/\/","https:\/\/", $play_list);
             // $str = str_replace("http://m","https://p", $str);
             $fo = fopen($save, "w+");
@@ -43,11 +43,10 @@
     } else if(!empty($_GET['lrc'])){
         header('Content-Type: text/plain;charset=UTF-8');
         
-        $lrc = $netease->lyric($_GET['lrc']);
-        
         $name = $_GET['lrc'] . ".lrc";
         $save = "cache/lrc/" . $name;
         if (!file_exists($save)) {
+            $lrc = $netease->lyric($_GET['lrc']);
             $str = json_decode($lrc,true);
             if(!empty($str['lyric'])){
                 $fo = fopen($save, "w");
